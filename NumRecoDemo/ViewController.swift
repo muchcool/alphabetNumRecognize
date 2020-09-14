350dc4ddab0a68fffd07953b2f8349af448a3960
muchcool <lano@scs-hqmdeimac.local>
2020-09-14 18:30:50 +0800

    Initial Commit

---
+++ NumRecoDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumRecoDemo
+//
+//  Created by lano on 2020/9/14.
+//  Copyright © 2020 SCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
